3c66f386a2c7ce0d8f2b8e46c71587b67c9974d3
Mads Skovgaard <madsskovgaard@hotmail.com>
2019-08-26 15:08:41 +0200

    Updates modinfo version.

--- ModInfo.cs
+++ ModInfo.cs
@@ -8,6 +8,6 @@ namespace WallPumps
     public static class ModInfo
     {
         public static string NAME = "Wall Pumps";
-        public static string VERSION = "1.0";
+        public static string VERSION = "1.1";
     }
 }
