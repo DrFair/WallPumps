2af6e01b12375a2578630ce2d280df83c888ce4f
Mads Skovgaard <madsskovgaard@hotmail.com>
2019-08-27 04:45:43 +0200

    Adds FairONI files for future use.

---
+++ FairONI/ConfigManager.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Harmony;
+
+namespace FairONI
+{
+    public static class ConfigManager
+    {
+
+    }
+    
+    [HarmonyPatch(typeof(ModsScreen), "OnActivate")]
+    internal class ModsScreen_OnSpawn_Patch
+    {
+
+        public static void PostFix(ModsScreen __instance)
+        {
+            // Change mods screen with config button?
+        }
+
+    }
+}
---
+++ FairONI/FairONI.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace FairONI
+{
+    public static class FairONI
+    {
+        // This will contain API functions for easier development
+    }
+}
