1d933d7cc85f14883a5a871cb08312c6d751954e
Mads Skovgaard <madsskovgaard@hotmail.com>
2019-08-28 23:31:52 +0200

    Add tag utils and cleans up imports.

--- FairONI/AddBuilding.cs
+++ FairONI/AddBuilding.cs
@@ -1,8 +1,6 @@
 ﻿using Database;
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using TUNING;
 
 namespace FairONI
---
+++ FairONI/ElementUtils.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+
+namespace FairONI
+{
+    public static class ElementUtils
+    {
+
+        public static void AddOreTag(Element element, Tag tag)
+        {
+            List<Tag> tags = new List<Tag>(element.oreTags) { tag };
+            element.oreTags = tags.ToArray();
+            GameTags.SolidElements.Add(element.tag);
+        }
+
+    }
+}
