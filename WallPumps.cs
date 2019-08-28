f7108bcd5fdfa876e98b4641d723ed9c27bf93b5
Mads Skovgaard <madsskovgaard@hotmail.com>
2019-08-28 23:38:44 +0200

    Adds new wall machine material tag.

--- WallPumps.cs
+++ WallPumps.cs
@@ -4,9 +4,15 @@ using System.Linq;
 using System.Text;
 using Harmony;
 using FairONI;
+using TUNING;
 
 namespace WallPumps
 {
+
+    public static class WallPumps
+    {
+        public static readonly Tag WallMachineMaterial = TagManager.Create("WallMachineMaterial");
+    }
 
     [HarmonyPatch(typeof(GeneratedBuildings))]
     [HarmonyPatch("LoadGeneratedBuildings")]
@@ -14,8 +20,28 @@ namespace WallPumps
     {
         public static void Prefix()
         {
+            AddTags.AddStrings(WallPumps.WallMachineMaterial, "Wall Machine Material");
+
             GasWallPump.Setup();
             LiquidWallPump.Setup();
+        }
+    }
+
+    [HarmonyPatch(typeof(ElementLoader))]
+    [HarmonyPatch("FinaliseElementsTable")]
+    public static class ElementLoader_FinaliseElementsTable_Patch
+    {
+        public static void Postfix()
+        {
+            // Add new material it to all refined metals + insulation
+            foreach (Element e in ElementLoader.elementTable.Values)
+            {
+                if (e.HasTag(GameTags.RefinedMetal))
+                {
+                    ElementUtils.AddOreTag(e, WallPumps.WallMachineMaterial);
+                }
+            }
+            ElementUtils.AddOreTag(ElementLoader.FindElementByHash(SimHashes.SuperInsulator), WallPumps.WallMachineMaterial);
         }
     }
 
